P3pp3rF1y/SophisticatedBackpacks

feat: Added REI compatibility

src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/compat/recipeviewers/rei/package-info.java
@@ -1,0 +1,8 @@
+// Auto generated package-info by MCP
+
+@ParametersAreNonnullByDefault @MethodsReturnNonnullByDefault
+package net.p3pp3rf1y.sophisticatedbackpacks.compat.recipeviewers.rei;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;

P3pp3rF1y/SophisticatedStorageInMotion

src/main/java/net/p3pp3rf1y/sophisticatedstorageinmotion/compat/recipeviewers/rei/package-info.java
@@ -1,0 +1,8 @@
+// Auto generated package-info by MCP
+
+@ParametersAreNonnullByDefault @MethodsReturnNonnullByDefault
+package net.p3pp3rf1y.sophisticatedstorageinmotion.compat.recipeviewers.rei;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;