da064cbaefe42dbed792dc1f75010904fe18bd39
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-06-26 09:17:26 +0200

    feat: ✨ Added paintbrush outlines to moving storage

--- src/main/java/net/p3pp3rf1y/sophisticatedstorageinmotion/client/ClientEventHandler.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorageinmotion/client/ClientEventHandler.java
@@ -1,14 +1,31 @@
 package net.p3pp3rf1y.sophisticatedstorageinmotion.client;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.client.event.RegisterClientTooltipComponentFactoriesEvent;
 import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
+import net.minecraftforge.client.event.RenderLevelStageEvent;
 import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.p3pp3rf1y.sophisticatedcore.client.render.BlockHighlightRenderHelper;
+import net.p3pp3rf1y.sophisticatedcore.util.InventoryHelper;
+import net.p3pp3rf1y.sophisticatedcore.util.VoxelOutliner;
 import net.p3pp3rf1y.sophisticatedstorageinmotion.client.gui.PaintbrushMovingStorageOverlay;
+import net.p3pp3rf1y.sophisticatedstorageinmotion.entity.IMovingStorageEntity;
 import net.p3pp3rf1y.sophisticatedstorageinmotion.item.MovingStorageItem;
 
 public class ClientEventHandler {
+	private static final int PAINTBRUSH_CAN_APPLY_HIGHLIGHT_COLOR = 0x69c53b;
+	private static final int PAINTBRUSH_MISSING_ITEMS_HIGHLIGHT_COLOR = 0xc53b3b;
+
 	private ClientEventHandler() {
 	}
 
@@ -18,6 +35,43 @@ public class ClientEventHandler {
 
 		IEventBus eventBus = MinecraftForge.EVENT_BUS;
 		eventBus.addListener(ClientMovingStorageContentsTooltip::onWorldLoad);
+		eventBus.addListener(ClientEventHandler::renderLevelStage);
+	}
+
+	private static void renderLevelStage(RenderLevelStageEvent event) {
+		if (event.getStage() != RenderLevelStageEvent.Stage.AFTER_BLOCK_ENTITIES) {
+			return;
+		}
+
+		renderPaintbrushEntityHighlight(event.getPoseStack(), event.getPartialTick(), event.getCamera().getPosition());
+	}
+
+	private static void renderPaintbrushEntityHighlight(PoseStack poseStack, float partialTick, Vec3 cameraPos) {
+		Minecraft mc = Minecraft.getInstance();
+		LocalPlayer player = mc.player;
+		if (player == null || mc.screen != null || mc.level == null || !(mc.hitResult instanceof EntityHitResult entityHitResult)
+				|| !(entityHitResult.getEntity() instanceof IMovingStorageEntity)) {
+			return;
+		}
+
+		InventoryHelper.getItemFromEitherHand(player, net.p3pp3rf1y.sophisticatedstorage.init.ModItems.PAINTBRUSH.get())
+				.flatMap(paintbrush -> PaintbrushMovingStorageOverlay.getItemRequirementsFor(paintbrush, player, entityHitResult.getEntity()))
+				.ifPresent(itemRequirements -> renderPaintbrushEntityHighlight(poseStack, partialTick, cameraPos, entityHitResult.getEntity(),
+						itemRequirements.itemsMissing().isEmpty()));
+	}
+
+	private static void renderPaintbrushEntityHighlight(PoseStack poseStack, float partialTick, Vec3 cameraPos, Entity entity, boolean canApply) {
+		AABB boundingBox = entity.getBoundingBox();
+		double x = Mth.lerp(partialTick, entity.xOld, entity.getX());
+		double y = Mth.lerp(partialTick, entity.yOld, entity.getY());
+		double z = Mth.lerp(partialTick, entity.zOld, entity.getZ());
+		int color = canApply ? PAINTBRUSH_CAN_APPLY_HIGHLIGHT_COLOR : PAINTBRUSH_MISSING_ITEMS_HIGHLIGHT_COLOR;
+		MultiBufferSource.BufferSource buffer = Minecraft.getInstance().renderBuffers().bufferSource();
+		poseStack.pushPose();
+		poseStack.translate(x - cameraPos.x(), y - cameraPos.y(), z - cameraPos.z());
+		BlockHighlightRenderHelper.renderThickEdges(poseStack, buffer, color, VoxelOutliner.edgesFromAABB(boundingBox), entity.getX(), entity.getY(),
+				entity.getZ());
+		poseStack.popPose();
 	}
 
 	private static void registerOverlay(RegisterGuiOverlaysEvent event) {
