49ea21c3f2c1b44157dda365a01e26861ae59d4d
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-08-19 23:27:29 +0200

    fix: 🐛 Fixed moving storage closing at extended reach

--- src/main/java/net/p3pp3rf1y/sophisticatedstorageinmotion/common/gui/MovingStorageContainerMenu.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstorageinmotion/common/gui/MovingStorageContainerMenu.java
@@ -137,7 +137,7 @@ public class MovingStorageContainerMenu<T extends Entity & IMovingStorageEntity>
 
 	@Override
 	public boolean stillValid(Player player) {
-		return getStorageEntity().map(se -> player.distanceToSqr(se.position()) <= 64.0D).orElse(false);
+		return getStorageEntity().map(se -> se.isAlive() && player.canInteractWithEntity(se, 4.0F)).orElse(false);
 	}
 
 	@Override
